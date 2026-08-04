854e22af395ac475842da4c7cba7bb4a02754961
Winged <lonely840314@gmail.com>
2026-08-04 13:27:17 +0800

    建立 MainForm 基本版面

--- TempleReceipt/Forms/MainForm.Designer.cs
+++ TempleReceipt/Forms/MainForm.Designer.cs
@@ -49,6 +49,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.grpReceipt.SuspendLayout();
             this.grpDonor.SuspendLayout();
             this.grpItems.SuspendLayout();
@@ -65,22 +72,32 @@
             // 
             // grpReceipt
             // 
+            this.grpReceipt.Controls.Add(this.label7);
+            this.grpReceipt.Controls.Add(this.label6);
+            this.grpReceipt.Controls.Add(this.label3);
+            this.grpReceipt.Controls.Add(this.label2);
             this.grpReceipt.Controls.Add(this.txtOperator);
             this.grpReceipt.Controls.Add(this.txtReceiptNo);
-            this.grpReceipt.Location = new System.Drawing.Point(3, 3);
+            this.grpReceipt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpReceipt.Location = new System.Drawing.Point(5, 5);
+            this.grpReceipt.Margin = new System.Windows.Forms.Padding(5);
             this.grpReceipt.Name = "grpReceipt";
-            this.grpReceipt.Size = new System.Drawing.Size(359, 93);
+            this.grpReceipt.Size = new System.Drawing.Size(392, 112);
             this.grpReceipt.TabIndex = 0;
             this.grpReceipt.TabStop = false;
             this.grpReceipt.Text = "收據資訊";
             // 
             // grpDonor
             // 
+            this.grpDonor.Controls.Add(this.label5);
+            this.grpDonor.Controls.Add(this.label4);
             this.grpDonor.Controls.Add(this.txtAddress);
             this.grpDonor.Controls.Add(this.txtName);
-            this.grpDonor.Location = new System.Drawing.Point(3, 102);
+            this.grpDonor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpDonor.Location = new System.Drawing.Point(3, 125);
             this.grpDonor.Name = "grpDonor";
-            this.grpDonor.Size = new System.Drawing.Size(394, 143);
+            this.grpDonor.Padding = new System.Windows.Forms.Padding(5);
+            this.grpDonor.Size = new System.Drawing.Size(396, 133);
             this.grpDonor.TabIndex = 1;
             this.grpDonor.TabStop = false;
             this.grpDonor.Text = "信徒資料";
@@ -88,20 +105,25 @@
             // grpItems
             // 
             this.grpItems.Controls.Add(this.dgvItems);
-            this.grpItems.Location = new System.Drawing.Point(3, 251);
+            this.grpItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpItems.Location = new System.Drawing.Point(3, 264);
             this.grpItems.Name = "grpItems";
-            this.grpItems.Size = new System.Drawing.Size(394, 363);
+            this.grpItems.Padding = new System.Windows.Forms.Padding(5);
+            this.grpItems.Size = new System.Drawing.Size(396, 387);
             this.grpItems.TabIndex = 2;
             this.grpItems.TabStop = false;
             this.grpItems.Text = "功德項目";
             // 
             // grpAmount
             // 
+            this.grpAmount.Controls.Add(this.label8);
             this.grpAmount.Controls.Add(this.lblChineseMoney);
             this.grpAmount.Controls.Add(this.lblTotal);
-            this.grpAmount.Location = new System.Drawing.Point(3, 625);
+            this.grpAmount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpAmount.Location = new System.Drawing.Point(5, 659);
+            this.grpAmount.Margin = new System.Windows.Forms.Padding(5);
             this.grpAmount.Name = "grpAmount";
-            this.grpAmount.Size = new System.Drawing.Size(359, 77);
+            this.grpAmount.Size = new System.Drawing.Size(392, 77);
             this.grpAmount.TabIndex = 3;
             this.grpAmount.TabStop = false;
             this.grpAmount.Text = "金額資訊";
@@ -111,39 +133,42 @@
             this.grpAction.Controls.Add(this.btnPrint);
             this.grpAction.Controls.Add(this.btnPreview);
             this.grpAction.Controls.Add(this.btnClear);
-            this.grpAction.Location = new System.Drawing.Point(3, 708);
+            this.grpAction.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpAction.Location = new System.Drawing.Point(5, 746);
+            this.grpAction.Margin = new System.Windows.Forms.Padding(5);
             this.grpAction.Name = "grpAction";
-            this.grpAction.Size = new System.Drawing.Size(359, 119);
+            this.grpAction.Size = new System.Drawing.Size(392, 123);
             this.grpAction.TabIndex = 3;
             this.grpAction.TabStop = false;
             this.grpAction.Text = "功能";
             // 
             // txtReceiptNo
             // 
-            this.txtReceiptNo.Location = new System.Drawing.Point(15, 27);
+            this.txtReceiptNo.Location = new System.Drawing.Point(87, 20);
             this.txtReceiptNo.Name = "txtReceiptNo";
             this.txtReceiptNo.Size = new System.Drawing.Size(119, 30);
             this.txtReceiptNo.TabIndex = 4;
             // 
             // txtOperator
             // 
-            this.txtOperator.Location = new System.Drawing.Point(191, 29);
+            this.txtOperator.Location = new System.Drawing.Point(87, 56);
             this.txtOperator.Name = "txtOperator";
             this.txtOperator.Size = new System.Drawing.Size(119, 30);
             this.txtOperator.TabIndex = 5;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(87, 29);
+            this.txtName.Location = new System.Drawing.Point(59, 25);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(119, 30);
             this.txtName.TabIndex = 5;
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(87, 65);
+            this.txtAddress.Location = new System.Drawing.Point(59, 65);
+            this.txtAddress.Multiline = true;
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(119, 30);
+            this.txtAddress.Size = new System.Drawing.Size(294, 60);
             this.txtAddress.TabIndex = 6;
             // 
             // dgvItems
@@ -152,11 +177,11 @@
             this.dgvItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dgvItems.Location = new System.Drawing.Point(59, 29);
+            this.dgvItems.Location = new System.Drawing.Point(13, 31);
             this.dgvItems.Name = "dgvItems";
             this.dgvItems.RowHeadersWidth = 51;
             this.dgvItems.RowTemplate.Height = 27;
-            this.dgvItems.Size = new System.Drawing.Size(294, 150);
+            this.dgvItems.Size = new System.Drawing.Size(305, 330);
             this.dgvItems.TabIndex = 0;
             // 
             // Column1
@@ -176,20 +201,20 @@
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(83, 26);
+            this.lblTotal.Location = new System.Drawing.Point(8, 26);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(58, 22);
+            this.lblTotal.Size = new System.Drawing.Size(73, 22);
             this.lblTotal.TabIndex = 0;
-            this.lblTotal.Text = "label1";
+            this.lblTotal.Text = "總金額 : ";
             // 
             // lblChineseMoney
             // 
             this.lblChineseMoney.AutoSize = true;
-            this.lblChineseMoney.Location = new System.Drawing.Point(83, 48);
+            this.lblChineseMoney.Location = new System.Drawing.Point(198, 26);
             this.lblChineseMoney.Name = "lblChineseMoney";
-            this.lblChineseMoney.Size = new System.Drawing.Size(58, 22);
+            this.lblChineseMoney.Size = new System.Drawing.Size(61, 22);
             this.lblChineseMoney.TabIndex = 1;
-            this.lblChineseMoney.Text = "label1";
+            this.lblChineseMoney.Text = "零元整";
             // 
             // btnClear
             // 
@@ -197,7 +222,7 @@
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(84, 44);
             this.btnClear.TabIndex = 0;
-            this.btnClear.Text = "button1";
+            this.btnClear.Text = "清空";
             this.btnClear.UseVisualStyleBackColor = true;
             // 
             // btnPreview
@@ -206,7 +231,7 @@
             this.btnPreview.Name = "btnPreview";
             this.btnPreview.Size = new System.Drawing.Size(84, 44);
             this.btnPreview.TabIndex = 1;
-            this.btnPreview.Text = "button2";
+            this.btnPreview.Text = "預覽";
             this.btnPreview.UseVisualStyleBackColor = true;
             // 
             // btnPrint
@@ -215,7 +240,7 @@
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(84, 44);
             this.btnPrint.TabIndex = 2;
-            this.btnPrint.Text = "button3";
+            this.btnPrint.Text = "列印";
             this.btnPrint.UseVisualStyleBackColor = true;
             // 
             // pnlPreview
@@ -225,7 +250,7 @@
             this.pnlPreview.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlPreview.Location = new System.Drawing.Point(0, 0);
             this.pnlPreview.Name = "pnlPreview";
-            this.pnlPreview.Size = new System.Drawing.Size(678, 832);
+            this.pnlPreview.Size = new System.Drawing.Size(684, 874);
             this.pnlPreview.TabIndex = 4;
             // 
             // label1
@@ -250,8 +275,8 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pnlPreview);
-            this.splitContainer1.Size = new System.Drawing.Size(1082, 832);
-            this.splitContainer1.SplitterDistance = 400;
+            this.splitContainer1.Size = new System.Drawing.Size(1090, 874);
+            this.splitContainer1.SplitterDistance = 402;
             this.splitContainer1.TabIndex = 5;
             // 
             // tableLayoutPanel1
@@ -267,20 +292,83 @@
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 832);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(402, 874);
             this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "收據編號";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "經手人";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "姓名";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "地址";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(212, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 22);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "時間";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(212, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 22);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "道曆";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(87, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 22);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "0元";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(1082, 832);
+            this.ClientSize = new System.Drawing.Size(1090, 874);
             this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -332,6 +420,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
     }
 }
 
